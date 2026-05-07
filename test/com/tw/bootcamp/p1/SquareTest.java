fbbb905b8bcb60ee4b058381fe8c78ca7864eb4a
Dasari Nandini <nandinidasari728@gmail.com>
2026-05-07 12:04:02 +0530

    adds  square class with calculateArea and calculatePerimeter

---
+++ test/com/tw/bootcamp/p1/SquareTest.java
@@ -0,0 +1,19 @@
+package com.tw.bootcamp.p1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquareTest {
+    @Test
+    void shouldCalculateArea() {
+        Square square = new Square(4);
+        assertEquals(16, square.calculateArea());
+    }
+
+    @Test
+    void shouldCalculatePerimeter() {
+        Square square = new Square(3.5f);
+        assertEquals(14, square.calculatePerimeter());
+    }
+}
