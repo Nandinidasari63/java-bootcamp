1cd0ad7b6f2dd464e5ece84b5266095217831bb3
Dasari Nandini <nandinidasari728@gmail.com>
2026-05-07 15:08:55 +0530

    adds Probability and calculateProbability method

---
+++ src/com/tw/bootcamp/p2/Probability.java
@@ -0,0 +1,7 @@
+package com.tw.bootcamp.p2;
+
+public class Probability {
+    public static float calculateProbability(int favourableOutcomes,int possibleOutcomes ){
+        return (float) favourableOutcomes /possibleOutcomes;
+    }
+}
